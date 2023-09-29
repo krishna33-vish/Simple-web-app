189d0cb9dbbfe85288caa88b468e83b552ebe1f0
Krishna Gaikwad <krishnagaikwad.krishna@sensiaglobal.com>
2023-09-29 20:15:34 +0530

    hii

---
+++ script.js
@@ -0,0 +1,24 @@
+document.addEventListener("DOMContentLoaded", function () {
+    const taskInput = document.getElementById("task");
+    const addTaskButton = document.getElementById("addTask");
+    const taskList = document.getElementById("taskList");
+
+    addTaskButton.addEventListener("click", function () {
+        const taskText = taskInput.value.trim();
+
+        if (taskText !== "") {
+            const listItem = document.createElement("li");
+            listItem.innerHTML = `
+                <span>${taskText}</span>
+                <button class="delete">Delete</button>
+            `;
+            taskList.appendChild(listItem);
+            taskInput.value = "";
+
+            // Delete task when the "Delete" button is clicked
+            listItem.querySelector(".delete").addEventListener("click", function () {
+                listItem.remove();
+            });
+        }
+    });
+});
